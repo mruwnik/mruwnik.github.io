4ee7fc5c7a7cfca4a036268e4ba9f1562a92a5b0
Daniel O'Connell <tojad99@gmail.com>
2020-05-29 00:56:52 +0200

    add dense and avgs

--- bociany/raw/converter.py
+++ bociany/raw/converter.py
@@ -21,24 +21,30 @@ def add_top_bottom(current, vals):
     return [bottom] + current + [top]
 
 
-def interval_values(key, intervals_map, model, intervals):
-    current = list(intervals_map[(model, str(intervals[0]))][key])
+def interval_values(key, intervals_map, model, intervals, m_type):
+    current = list(intervals_map[(model, str(intervals[0]), m_type)][key])
     for interval in intervals[1:]:
-        bottom, avg, top = intervals_map[(model, str(interval))][key]
+        bottom, avg, top = intervals_map[(model, str(interval), m_type)][key]
         current = [bottom] + current + [top]
     return current
 
 
-def all_values(height, diam, models_values, models):
-    return [
-        interval_values((diam, height, 'normal'), models_values, model, intervals)
+def all_values(height, diam, models_values, models, m_type):
+    weights = [0.478365, 0.353365, 0.168269]
+    values = [
+        interval_values((diam, height, m_type), models_values, model, intervals, m_type)
         for model, intervals in models.items()
     ]
+    avgs = [
+        sum(map(lambda v: v[0] * v[1], zip(vals, weights)))
+        for vals in zip(*values)
+    ]
+    return values + [avgs]
 
 
 def model_vals():
     models_values = {
-        tuple(filename.name.split('.')[:-1]): file_vals(filename) for filename in Path().glob('*.txt')
+        tuple(filename.name.replace('_', '.').split('.')[:-1]): file_vals(filename) for filename in Path().glob('*.txt')
     }
     models = {
         model: sorted(int(i[1]) for i in intervals)
@@ -46,12 +52,12 @@ def model_vals():
     }
 
     return {
-        1: {
+        t: {
             diam: {
-                height: all_values(height, diam, models_values, models)
+                height: all_values(height, diam, models_values, models, m_type)
                 for height in range(10, 140)
             } for diam in range(80, 230)
-        }
+        } for t, m_type in enumerate(['normal', 'dense'], 1)
     }
 
 
